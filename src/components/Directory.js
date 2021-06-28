191c8c4c96a36b015501d59a0b27d40b7d8140d2
Tim O'Malley <tomalley24@gmail.com>
2021-06-28 16:01:55 -0500

    search bar now filters employees

--- src/components/Directory.js
+++ src/components/Directory.js
@@ -19,11 +19,21 @@ class Directory extends React.Component {
         .catch(err => console.log(err));
     }
 
+    handleInputChange = e => {
+        const searchFilter = e.target.value;
+
+        let employeeFilter = this.state.employeeList.filter((filter) => {
+            let values = Object.values(filter).join('').toLowerCase();
+            return values.indexOf(searchFilter.toLowerCase()) != -1
+        });
+        this.setState({employeeFilter: employeeFilter});
+    } 
+
     render(){
         return(
             <div>
                 <h1 className = "text-3xl text-center px-5 py-5">List of Employees</h1>
-                <Searchbar />
+                <Searchbar handleInputChange = {this.handleInputChange}/>
                 <Table employeeList={this.state.employeeFilter}/>
             </div>
         )
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -6,7 +6,13 @@ function Searchbar(props){
             <p className = "text-center py-2">Search for employee by name, email, or number</p>
             <div className="p-8">
                 <div className="bg-white flex items-center rounded-full shadow-xl">
-                    <input className="rounded-l-full w-full py-4 px-6 text-gray-700 leading-tight focus:outline-none" id="search" type="text" placeholder="Search"/>
+                    <input 
+                        className="rounded-l-full w-full py-4 px-6 text-gray-700 leading-tight focus:outline-none" 
+                        id="search" 
+                        type="text" 
+                        placeholder="Search"
+                        onChange = {props.handleInputChange}
+                    />
                     <div className="p-4"></div>
                 </div>
             </div>
